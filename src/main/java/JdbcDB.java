7824e7ee6aca2c3d3ee3844b0f1a31a2cb697065
FabiBr <fabian.br.1992@gmail.com>
2014-09-30 12:46:36 +0200

    new Structure of the Server Project; recreated with maven to make it runnable on the actual server

---
+++ src/main/java/JdbcDB.java
@@ -0,0 +1,184 @@
+package main.java;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+
+
+public class JdbcDB {
+
+	private static final String datapath = "jdbc:sqlite:test.db";
+	
+	// usersTableKeys
+	private static final String USER_TABLE = "users";
+
+	private static final String USER_ID_KEY = "id";
+	private static final String USER_NAME_KEY = "name";
+	private static final String USER_PW_KEY = "pw";
+	private static final String USER_GWON_KEY = "gamesWon";
+	private static final String USER_GLOST_KEY = "gamesLost";
+	private static final String USER_PREMIUM_KEY = "premium";
+
+	// GamesTableKeys
+
+	private static final String GAMES_TABLE = "games";
+
+	private static final String GAMES_ID_KEY = "id";
+	private static final String GAMES_FIELD_KEY = "field";
+	private static final String GAMES_P1_KEY = "player1";
+	private static final String GAMES_P2_KEY = "player2";
+	private static final String GAMES_LASTPLAYER_KEY = "lastPlayer";
+
+	Connection c = null;
+	Statement stmt = null;
+
+	public JdbcDB() {
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection(datapath);
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			String CREATE_USER_TABLE = "CREATE TABLE IF NOT EXISTS "
+					+ USER_TABLE + " (" + USER_ID_KEY
+					+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + USER_NAME_KEY
+					+ " TEXT, " + USER_PW_KEY + " TEXT, " + USER_GWON_KEY
+					+ " INTEGER, " + USER_GLOST_KEY + " INTEGER, "
+					+ USER_PREMIUM_KEY + " INTEGER)";
+			String CREATE_GAMES_TABLE = "CREATE TABLE IF NOT EXISTS "
+					+ GAMES_TABLE + " (" + GAMES_ID_KEY
+					+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + GAMES_FIELD_KEY
+					+ " TEXT, " + GAMES_P1_KEY + " TEXT, " + GAMES_P2_KEY
+					+ " TEXT, " + GAMES_LASTPLAYER_KEY + " TEXT)";
+			stmt.executeUpdate(CREATE_USER_TABLE);
+			stmt.executeUpdate(CREATE_GAMES_TABLE);
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Table created successfully");
+	}
+	
+	public void insertNewGame(String field, String user1, String user2, String currentUser) throws SQLException {
+		c = DriverManager.getConnection(datapath);
+	    stmt = c.createStatement();
+	      
+	    String sql = "INSERT INTO " + GAMES_TABLE +  "(" + GAMES_FIELD_KEY + "," + GAMES_P1_KEY + ","
+		  			+ GAMES_P2_KEY + "," + GAMES_LASTPLAYER_KEY + ")" + "VALUES ('" + field + "', '" + user1 + "', '" + user2 + "', '" + currentUser + "')";
+	    System.out.println(sql);
+	    stmt.executeUpdate(sql);
+	    System.out.println("update succesful");
+	    stmt.close();
+	    c.close();
+		
+		
+	}
+
+	public void insertNewUser(String username, String userPw) throws SQLException {
+		c = DriverManager.getConnection(datapath);
+	    stmt = c.createStatement();
+	      
+	    String sql = "INSERT INTO USERS (" + USER_NAME_KEY + "," + USER_PW_KEY + ","
+		  			+ USER_GWON_KEY + "," + USER_GLOST_KEY + "," + USER_PREMIUM_KEY + ")" + "VALUES ('" + username + "', '" + userPw + "', '0', '0', '0')";
+	    System.out.println(sql);
+	    stmt.executeUpdate(sql);
+	    System.out.println("update succesful");
+	    stmt.close();
+	    c.close();
+	}
+
+	public String getPwByUsername(String username1) throws SQLException {
+		c = DriverManager.getConnection(datapath);
+	    stmt = c.createStatement();
+	    
+	    String sql = "SELECT " + USER_PW_KEY + " FROM " + USER_TABLE + " WHERE " + USER_NAME_KEY + "=" + "'" + username1 + "'";
+	    ResultSet rs = stmt.executeQuery(sql);
+	    
+	    
+	    boolean isEmpty = true;
+	    while(rs.next()) {
+	    	isEmpty = false;
+	    }
+	    if(isEmpty) {
+	    	stmt.close();
+		    c.close();
+	    	return "";
+	    } else {
+	    	rs = stmt.executeQuery(sql);
+	    	String pw = rs.getString(USER_PW_KEY);
+	    	stmt.close();
+		    c.close();
+	    	return pw;
+	    }
+	}
+
+	public ArrayList<String[]> getAllUsers() throws SQLException {
+		ArrayList<String[]> users = new ArrayList<>();
+		c = DriverManager.getConnection(datapath);
+	    stmt = c.createStatement();
+	    
+	    String sql = "SELECT * FROM " + USER_TABLE;
+	    ResultSet rs = stmt.executeQuery(sql);
+	    while ( rs.next() ) {
+	    	
+	    	String[] user = new String[rs.getMetaData().getColumnCount()+1];
+	    	user[1] = String.valueOf(rs.getInt(USER_ID_KEY));
+	    	user[2] = rs.getString(USER_NAME_KEY);
+	    	user[3] = rs.getString(USER_PW_KEY);
+	    	user[4] = String.valueOf(rs.getInt(USER_GWON_KEY));
+	    	user[5] = String.valueOf(rs.getInt(USER_GLOST_KEY));
+	    	user[6] = String.valueOf(rs.getInt(USER_PREMIUM_KEY));
+	    	users.add(user);
+	      }
+	    stmt.close();
+	    c.close();
+		return users;
+	}
+	
+	public void createNewGame(String field, String user1, String user2) throws SQLException {
+		c = DriverManager.getConnection(datapath);
+	    stmt = c.createStatement();
+	    
+	    String sql = "INSERT INTO GAMES (" + GAMES_FIELD_KEY + "," + GAMES_P1_KEY + ","
+	  			+ GAMES_P2_KEY + "," + GAMES_LASTPLAYER_KEY + ")" + "VALUES ('" + field + "', '" + user1 + "', '" + user2 + "', '" + user1 + "')";
+	    stmt.executeUpdate(sql);
+	    stmt.close();
+	    c.close();	
+	}
+
+}
+
+/*
+ * String CREATE_USER_TABLE = "create table if not exists" + USER_TABLE + " (" +
+ * USER_ID_KEY + " integer primary key autoincrement, " + USER_NAME_KEY +
+ * " text, " + USER_PW_KEY + " text, " + USER_GWON_KEY + " integer, " +
+ * USER_GLOST_KEY + " integer, " + USER_PREMIUM_KEY + " integer)"; String
+ * CREATE_GAMES_TABLE = "create table if not exists" + GAMES_TABLE + " (" +
+ * GAMES_ID_KEY + " integer primary key autoincrement, " + GAMES_FIELD_KEY +
+ * " text, " + GAMES_P1_KEY + " integer, " + GAMES_P2_KEY + " integer, " +
+ * GAMES_LASTPLAYER_KEY + " integer)";
+ */
+
+/*
+ * public static void main(String args[]) { Connection c = null; Statement stmt
+ * = null; try { Class.forName("org.sqlite.JDBC"); c =
+ * DriverManager.getConnection("jdbc:sqlite:test.db");
+ * System.out.println("Opened database successfully");
+ * 
+ * stmt = c.createStatement(); String CREATE_USER_TABLE =
+ * "create table if not exists" + USER_TABLE + " (" + USER_ID_KEY +
+ * " integer primary key autoincrement, " + USER_NAME_KEY + " text, " +
+ * USER_PW_KEY + " text, " + USER_GWON_KEY + " integer, " + USER_GLOST_KEY +
+ * " integer, " + USER_PREMIUM_KEY + " integer)"; String CREATE_GAMES_TABLE =
+ * "create table if not exists" + GAMES_TABLE + " (" + GAMES_ID_KEY +
+ * " integer primary key autoincrement, " + GAMES_FIELD_KEY + " text, " +
+ * GAMES_P1_KEY + " integer, " + GAMES_P2_KEY + " integer, " +
+ * GAMES_LASTPLAYER_KEY + " integer)"; stmt.executeUpdate(CREATE_USER_TABLE);
+ * stmt.executeUpdate(CREATE_GAMES_TABLE); stmt.close(); c.close(); } catch
+ * (Exception e) { System.err.println(e.getClass().getName() + ": " +
+ * e.getMessage()); System.exit(0); }
+ * System.out.println("Table created successfully"); } }
+ */
---
+++ src/main/java/Server.java
@@ -0,0 +1,202 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import org.json.*;
+
+
+
+
+public class Server extends Thread {
+
+	private static ServerSocket serverSocket = null;
+	public static final int SERVERPORT = 4444;
+	private static JdbcDB myDb = new JdbcDB();
+
+	private static final String INSERT_NEWUSER_KEY = "newUserInsert";
+	private static final String INSERT_GAME_KEY = "newGameInsert";
+	private static final String INCREMENT_USER_WIN_KEY = "userWins";
+	private static final String PASSWORD_CHECK_KEY = "checkPw";
+	private static final String GET_ALL_USERS_KEY = "allUsersGet";
+	private static final String GET_GAMES_KEY = "myGamesGet";
+	
+
+	public static void main(String[] args) throws IOException {
+		
+		Thread server = new Thread(new ServerThread());
+		server.start();
+
+	}
+
+	static class ServerThread implements Runnable {
+
+		public void run() {
+			Socket client = null;
+			try {
+				serverSocket = new ServerSocket(SERVERPORT);
+				System.out.println("server is listening");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					client = serverSocket.accept();
+
+					CommunicationThread commThread = new CommunicationThread(
+							client);
+					new Thread(commThread).start();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	static class CommunicationThread implements Runnable {
+
+		private Socket clientSocket;
+
+		private BufferedReader input;
+		private PrintWriter output;
+
+		public CommunicationThread(Socket clientSocket) {
+
+			this.clientSocket = clientSocket;
+
+			try {
+
+				this.input = new BufferedReader(new InputStreamReader(
+						this.clientSocket.getInputStream()));
+				
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+
+			//while (!Thread.currentThread().isInterrupted()) {
+
+				try {
+
+					String read = input.readLine();
+					String callback = handleConnection(read);
+					output = new PrintWriter(
+							new BufferedWriter(new OutputStreamWriter(
+									clientSocket.getOutputStream())), true);
+					output.println(callback);
+					output.flush();
+					System.out.println(read);
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			//}
+		}
+
+		private String handleConnection(String read) throws SQLException {
+			StringTokenizer tok = new StringTokenizer(read);
+			ArrayList<String> data = new ArrayList<String>();
+			while (tok.hasMoreElements()) {
+				data.add(tok.nextToken());
+			}
+			String key = data.get(0);
+
+			switch (key) {
+			case INSERT_NEWUSER_KEY:
+				String username = data.get(1);
+				String pw = data.get(2);
+				myDb.insertNewUser(username, pw);
+				return "User succesfully created";
+
+			case INSERT_GAME_KEY:
+				String field = data.get(1);
+				String user1 = data.get(2);
+				String user2 = data.get(3);
+				String currentUser = data.get(4);
+				myDb.insertNewGame(field, user1, user2, currentUser);
+				return "game created";
+
+			case INCREMENT_USER_WIN_KEY:
+
+				return null;
+			
+			case GET_ALL_USERS_KEY:
+				ArrayList<String[]> allUsersData = myDb.getAllUsers();
+				JSONArray list = new JSONArray(allUsersData);
+				String out = list.toString();
+				System.out.println(out);
+				return out;
+				
+			case PASSWORD_CHECK_KEY:
+				
+				String username1 = data.get(1);
+				String savedPw = myDb.getPwByUsername(username1);
+				if(savedPw.equals(data.get(2))) {
+					return "1";
+				}
+				return "Wrong password";
+
+
+			default:
+				return null;
+			}
+		}
+
+	}
+
+	/*
+	 * private Database db = new Database();
+	 * 
+	 * public void run() { ServerSocket sSocket = null;
+	 * 
+	 * try { sSocket = new ServerSocket(4444);
+	 * System.out.println("server is listening"); } catch (IOException e1) { //
+	 * TODO Auto-generated catch block e1.printStackTrace(); } while (true) {
+	 * Socket client = null;
+	 * 
+	 * try { //System.out.println("Client connected and handled"); client =
+	 * sSocket.accept(); handleConnection(client);
+	 * System.out.println("Client connected and handled"); } catch (IOException
+	 * e) { e.printStackTrace(); } finally { if (client != null) try {
+	 * client.close(); } catch (IOException e) { } } }
+	 * 
+	 * }
+	 * 
+	 * /*public static void startListening() throws IOException { (new
+	 * Server()).start(); }
+	 * 
+	 * public static void handleConnection(Socket client) throws IOException {
+	 * Scanner in = new Scanner(client.getInputStream()); PrintWriter out = new
+	 * PrintWriter(client.getOutputStream(), true); String sqlQuery =
+	 * in.nextLine(); System.out.println("something happened");
+	 * System.out.println(sqlQuery);
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * }
+	 * 
+	 * public static void main(String[] args) throws IOException { (new
+	 * Server()).start(); }
+	 */
+
+}
